d7a52923a612daa0980593e01a69f03a22c5e429
Tohka <1324419148@qq.com>
2024-03-30 19:47:35 +0800

    style(pinia的安装): 用变量pinia接收createPinia函数

--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
+const pinia=createPinia();
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus);
 app.mount('#app')
