6923c85573e306bd3e224c9e54e9e5d080f15063
Tohka <1324419148@qq.com>
2024-04-02 20:17:29 +0800

    feat: 完成显示航班对应的飞机票

--- src/views/ticketResult.vue
+++ src/views/ticketResult.vue
@@ -3,7 +3,7 @@
         <ul>
             <li class="ticketCardPos" style="margin: 10px 0">
                 <div style="font-size: 24px;color: #333;font-weight: 700">
-                    {{flight.startCity}}
+                    {{flight.departCity}}
                 </div>
                 <svg t="1710934823512" style="margin: 0 5px;" viewBox="0 0 6016 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5180" width="32" height="32"><path d="M5043.2 768V179.2c0-76.8 51.2-128 128-128 38.4 0 64 12.8 89.6 38.4l716.8 716.8c51.2 51.2 51.2 128 0 179.2-25.6 25.6-51.2 38.4-89.6 38.4H179.2c-76.8 0-128-51.2-128-128s51.2-128 128-128h4864z" fill="#333333" p-id="5181"></path></svg>
                 <div style="font-size: 24px;color: #333;font-weight: 700">
@@ -64,29 +64,29 @@
         <div class="container">
             <div class="flight-list">
 <!--                飞机票-->
-                <div class="flight-item">
+                <div class="flight-item" v-for="ticket in flightTickets" :key="ticket.flightNo">
                     <div class="flight-box">
                         <div class="flight-airline">
                             <div class="airline-logo">
                                 <img src="../assets/img/airlines/3U.png" alt="" width="48px" height="48px">
                             </div>
                             <ul>
-                                <li class="airline-name"><span>海南航空</span></li>
-                                <li class="plane"><span>HU7723 波音737(中)</span></li>
+                                <li class="airline-name"><span>{{ticket.airlineCompanyName}}</span></li>
+                                <li class="plane"><span>{{ticket.flightNo}} {{ticket.aircraftType}}</span></li>
                             </ul>
 
                         </div>
                         <div class="flight-detail">
                             <div class="depart-box">
-                                <div class="time">23:10</div>
-                                <div class="airport">宝安国际机场</div>
+                                <div class="time">{{ticket.departTime}}</div>
+                                <div class="airport">{{ ticket.departPortName }}</div>
                             </div>
                             <div class="arrow-box">
                                 <img src="../assets/img/component/to.png" alt="" width="120px" >
                             </div>
                             <div class="arrive-box">
-                                <div class="time">01:25</div>
-                                <div class="airport">浦东国际机场</div>
+                                <div class="time">{{ ticket.arriveTime }}</div>
+                                <div class="airport">{{ ticket.arrivePortName }}</div>
                             </div>
                         </div>
                         <div class="flight-tag">
@@ -103,237 +103,7 @@
                         <div class="flight-operate">
                             <div class="flight-price">
                                 <span>￥</span>
-                                <span style="font-size: 32px" >830</span>
-                            </div>
-                            <div class="flight-action">
-                                <button>订票</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flight-item">
-                    <div class="flight-box">
-                        <div class="flight-airline">
-                            <div class="airline-logo">
-                                <img src="../assets/img/airlines/A6.png" alt="" width="48px" height="48px">
-                            </div>
-                            <ul>
-                                <li class="airline-name"><span>东方航空</span></li>
-                                <li class="plane"><span>MU725 空客321(中)</span></li>
-                            </ul>
-
-                        </div>
-                        <div class="flight-detail">
-                            <div class="depart-box">
-                                <div class="time">21:20</div>
-                                <div class="airport">浦东国际机场</div>
-                            </div>
-                            <div class="arrow-box">
-                                <img src="../assets/img/component/to.png" alt="" width="120px" >
-                            </div>
-                            <div class="arrive-box">
-                                <div class="time">00:15</div>
-                                <div class="airport">香港国际机场</div>
-                            </div>
-                        </div>
-                        <div class="flight-tag">
-                            <el-tooltip effect="light">
-                                <template #default><span>租车新客￥788满减券</span></template>
-                                <template #content>
-                                    <p style="width: 378px;height: 274px;font-size: 12px">
-                                        （1）使用范围：适用于携程租车频道，以在线预付方式预订国内租车产品可用（部分供应商产品不可用，具体以列表页展示为准）； （2）券有效期：优惠券领取后7天内有效，过期自动失效； （3）门槛阶梯：可享租车费满100-12/满200-24/满400-48/满800-96/满1000-120/满6000-788元优惠（优惠金额仅限租车费，不含手续费、基础服务费、优享服务费、异地还车费等费用 ）； （4）使用限制：每张优惠券仅限使用1次，仅限本用户下单使用，不拆分、不找零、不转赠、不可与其他优惠活动及优惠券同享；已完成支付的订单不能使用此券； （5）退赔说明：使用券的订单无损取消、超时未支付、购买失败或因订单变更导致订单金额未满足优惠门槛的，若该券尚未失效将退回原账户，若该券已失效不予退回；如订单支持变更，已用券的订单因变更导致订单金额增加的，仅可享受变更前的阶梯优惠金额；已用券的订单如取消且产生取消费用的，优惠券因涉及抵扣（或部分抵扣）将不予退回。
-                                    </p>
-                                </template>
-                            </el-tooltip>
-                        </div>
-                        <div class="flight-operate">
-                            <div class="flight-price">
-                                <span>￥</span>
-                                <span style="font-size: 32px" >4943</span>
-                            </div>
-                            <div class="flight-action">
-                                <button>订票</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flight-item">
-                    <div class="flight-box">
-                        <div class="flight-airline">
-                            <div class="airline-logo">
-                                <img src="../assets/img/airlines/DR.png" alt="" width="48px" height="48px">
-                            </div>
-                            <ul>
-                                <li class="airline-name"><span>国泰航空</span></li>
-                                <li class="plane"><span>CX831 空客330(大)</span></li>
-                            </ul>
-
-                        </div>
-                        <div class="flight-detail">
-                            <div class="depart-box">
-                                <div class="time">19:00</div>
-                                <div class="airport">浦东国际机场</div>
-                            </div>
-                            <div class="arrow-box">
-                                <img src="../assets/img/component/to.png" alt="" width="120px" >
-                            </div>
-                            <div class="arrive-box">
-                                <div class="time">21:55</div>
-                                <div class="airport">香港国际机场</div>
-                            </div>
-                        </div>
-                        <div class="flight-tag">
-                            <el-tooltip effect="light">
-                                <template #default><span>租车新客￥788满减券</span></template>
-                                <template #content>
-                                    <p style="width: 378px;height: 274px;font-size: 12px">
-                                        （1）使用范围：适用于携程租车频道，以在线预付方式预订国内租车产品可用（部分供应商产品不可用，具体以列表页展示为准）； （2）券有效期：优惠券领取后7天内有效，过期自动失效； （3）门槛阶梯：可享租车费满100-12/满200-24/满400-48/满800-96/满1000-120/满6000-788元优惠（优惠金额仅限租车费，不含手续费、基础服务费、优享服务费、异地还车费等费用 ）； （4）使用限制：每张优惠券仅限使用1次，仅限本用户下单使用，不拆分、不找零、不转赠、不可与其他优惠活动及优惠券同享；已完成支付的订单不能使用此券； （5）退赔说明：使用券的订单无损取消、超时未支付、购买失败或因订单变更导致订单金额未满足优惠门槛的，若该券尚未失效将退回原账户，若该券已失效不予退回；如订单支持变更，已用券的订单因变更导致订单金额增加的，仅可享受变更前的阶梯优惠金额；已用券的订单如取消且产生取消费用的，优惠券因涉及抵扣（或部分抵扣）将不予退回。
-                                    </p>
-                                </template>
-                            </el-tooltip>
-                        </div>
-                        <div class="flight-operate">
-                            <div class="flight-price">
-                                <span>￥</span>
-                                <span style="font-size: 32px" >2375</span>
-                            </div>
-                            <div class="flight-action">
-                                <button>订票</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flight-item">
-                    <div class="flight-box">
-                        <div class="flight-airline">
-                            <div class="airline-logo">
-                                <img src="../assets/img/airlines/9C.png" alt="" width="48px" height="48px">
-                            </div>
-                            <ul>
-                                <li class="airline-name"><span>四川航空</span></li>
-                                <li class="plane"><span>3U8966 空客319(中)</span></li>
-                            </ul>
-
-                        </div>
-                        <div class="flight-detail">
-                            <div class="depart-box">
-                                <div class="time">19:55</div>
-                                <div class="airport">浦东国际机场</div>
-                            </div>
-                            <div class="arrow-box">
-                                <img src="../assets/img/component/to.png" alt="" width="120px" >
-                            </div>
-                            <div class="arrive-box">
-                                <div class="time">11:45</div>
-                                <div class="airport">香港国际机场</div>
-                            </div>
-                        </div>
-                        <div class="flight-tag">
-                            <el-tooltip effect="light">
-                                <template #default><span>租车新客￥788满减券</span></template>
-                                <template #content>
-                                    <p style="width: 378px;height: 274px;font-size: 12px">
-                                        （1）使用范围：适用于携程租车频道，以在线预付方式预订国内租车产品可用（部分供应商产品不可用，具体以列表页展示为准）； （2）券有效期：优惠券领取后7天内有效，过期自动失效； （3）门槛阶梯：可享租车费满100-12/满200-24/满400-48/满800-96/满1000-120/满6000-788元优惠（优惠金额仅限租车费，不含手续费、基础服务费、优享服务费、异地还车费等费用 ）； （4）使用限制：每张优惠券仅限使用1次，仅限本用户下单使用，不拆分、不找零、不转赠、不可与其他优惠活动及优惠券同享；已完成支付的订单不能使用此券； （5）退赔说明：使用券的订单无损取消、超时未支付、购买失败或因订单变更导致订单金额未满足优惠门槛的，若该券尚未失效将退回原账户，若该券已失效不予退回；如订单支持变更，已用券的订单因变更导致订单金额增加的，仅可享受变更前的阶梯优惠金额；已用券的订单如取消且产生取消费用的，优惠券因涉及抵扣（或部分抵扣）将不予退回。
-                                    </p>
-                                </template>
-                            </el-tooltip>
-                        </div>
-                        <div class="flight-operate">
-                            <div class="flight-price">
-                                <span>￥</span>
-                                <span style="font-size: 32px" >4060</span>
-                            </div>
-                            <div class="flight-action">
-                                <button>订票</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flight-item">
-                    <div class="flight-box">
-                        <div class="flight-airline">
-                            <div class="airline-logo">
-                                <img src="../assets/img/airlines/3U.png" alt="" width="48px" height="48px">
-                            </div>
-                            <ul>
-                                <li class="airline-name"><span>海南航空</span></li>
-                                <li class="plane"><span>HU7723 波音737(中)</span></li>
-                            </ul>
-
-                        </div>
-                        <div class="flight-detail">
-                            <div class="depart-box">
-                                <div class="time">23:10</div>
-                                <div class="airport">宝安国际机场</div>
-                            </div>
-                            <div class="arrow-box">
-                                <img src="../assets/img/component/to.png" alt="" width="120px" >
-                            </div>
-                            <div class="arrive-box">
-                                <div class="time">01:25</div>
-                                <div class="airport">浦东国际机场</div>
-                            </div>
-                        </div>
-                        <div class="flight-tag">
-                            <el-tooltip effect="light">
-                                <template #default><span>租车新客￥788满减券</span></template>
-                                <template #content>
-                                    <p style="width: 378px;height: 274px;font-size: 12px">
-                                        （1）使用范围：适用于携程租车频道，以在线预付方式预订国内租车产品可用（部分供应商产品不可用，具体以列表页展示为准）； （2）券有效期：优惠券领取后7天内有效，过期自动失效； （3）门槛阶梯：可享租车费满100-12/满200-24/满400-48/满800-96/满1000-120/满6000-788元优惠（优惠金额仅限租车费，不含手续费、基础服务费、优享服务费、异地还车费等费用 ）； （4）使用限制：每张优惠券仅限使用1次，仅限本用户下单使用，不拆分、不找零、不转赠、不可与其他优惠活动及优惠券同享；已完成支付的订单不能使用此券； （5）退赔说明：使用券的订单无损取消、超时未支付、购买失败或因订单变更导致订单金额未满足优惠门槛的，若该券尚未失效将退回原账户，若该券已失效不予退回；如订单支持变更，已用券的订单因变更导致订单金额增加的，仅可享受变更前的阶梯优惠金额；已用券的订单如取消且产生取消费用的，优惠券因涉及抵扣（或部分抵扣）将不予退回。
-                                    </p>
-                                </template>
-                            </el-tooltip>
-                        </div>
-                        <div class="flight-operate">
-                            <div class="flight-price">
-                                <span>￥</span>
-                                <span style="font-size: 32px" >830</span>
-                            </div>
-                            <div class="flight-action">
-                                <button>订票</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flight-item">
-                    <div class="flight-box">
-                        <div class="flight-airline">
-                            <div class="airline-logo">
-                                <img src="../assets/img/airlines/3U.png" alt="" width="48px" height="48px">
-                            </div>
-                            <ul>
-                                <li class="airline-name"><span>海南航空</span></li>
-                                <li class="plane"><span>HU7723 波音737(中)</span></li>
-                            </ul>
-
-                        </div>
-                        <div class="flight-detail">
-                            <div class="depart-box">
-                                <div class="time">23:10</div>
-                                <div class="airport">宝安国际机场</div>
-                            </div>
-                            <div class="arrow-box">
-                                <img src="../assets/img/component/to.png" alt="" width="120px" >
-                            </div>
-                            <div class="arrive-box">
-                                <div class="time">01:25</div>
-                                <div class="airport">浦东国际机场</div>
-                            </div>
-                        </div>
-                        <div class="flight-tag">
-                            <el-tooltip effect="light">
-                                <template #default><span>租车新客￥788满减券</span></template>
-                                <template #content>
-                                    <p style="width: 378px;height: 274px;font-size: 12px">
-                                        （1）使用范围：适用于携程租车频道，以在线预付方式预订国内租车产品可用（部分供应商产品不可用，具体以列表页展示为准）； （2）券有效期：优惠券领取后7天内有效，过期自动失效； （3）门槛阶梯：可享租车费满100-12/满200-24/满400-48/满800-96/满1000-120/满6000-788元优惠（优惠金额仅限租车费，不含手续费、基础服务费、优享服务费、异地还车费等费用 ）； （4）使用限制：每张优惠券仅限使用1次，仅限本用户下单使用，不拆分、不找零、不转赠、不可与其他优惠活动及优惠券同享；已完成支付的订单不能使用此券； （5）退赔说明：使用券的订单无损取消、超时未支付、购买失败或因订单变更导致订单金额未满足优惠门槛的，若该券尚未失效将退回原账户，若该券已失效不予退回；如订单支持变更，已用券的订单因变更导致订单金额增加的，仅可享受变更前的阶梯优惠金额；已用券的订单如取消且产生取消费用的，优惠券因涉及抵扣（或部分抵扣）将不予退回。
-                                    </p>
-                                </template>
-                            </el-tooltip>
-                        </div>
-                        <div class="flight-operate">
-                            <div class="flight-price">
-                                <span>￥</span>
-                                <span style="font-size: 32px" >830</span>
+                                <span style="font-size: 32px" >{{ ticket.price }}</span>
                             </div>
                             <div class="flight-action">
                                 <button>订票</button>
@@ -347,13 +117,23 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
 import {getFormattedDate} from "@/utils/dateFormat.js";
+import {getFlightTickets} from "@/api/flight.js";
+
 const route=useRoute();
 const flight=route.query;//飞机航线
 const sortbarItems=[{label:"低价优先",id:"1"},{label: "起飞时间早-晚",id:"2"}];//排序项
 const currentIndex=ref(undefined);//当前选择排序项的索引
+const flightTickets=ref([]);//飞机票
+onMounted(()=>{
+    getFlightTickets(flight.departCity,flight.arriveCity).then(res=>{
+        console.log("getFlgihtTickets返回的数据",res);
+        let {data}=res.data;
+        flightTickets.value=data;
+    })
+})
 function toggleClass(index)
 {
     currentIndex.value=index;
@@ -445,6 +225,7 @@ function toggleClass(index)
     padding-left: 40px;
     padding-top: 24px;
     padding-bottom: 13px;
+    width: 215px;
 }
 .flight-box .flight-airline ul
 {
