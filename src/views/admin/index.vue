9ad7e7f96529ad762697e839c3cc5910a466f35a
Tohka <1324419148@qq.com>
2024-04-02 20:25:49 +0800

    feat: 实现折叠侧边栏（有bug）

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,22 +1,32 @@
 <script setup>
-import {ref} from "vue";
+import {ref, onMounted, onBeforeMount} from "vue";
 import {useRoute, useRouter} from "vue-router";
-let currentIndex=ref(0);
+import {emitter} from "@/utils/mitt.js";
+let currentIndex=ref(0);//侧边栏项索引
+let asideWidth=ref(150);
 const router=useRouter();
-const route=useRoute();
-console.log(router);
-console.log(route);
 function ToRoute(path,index)
 {
     console.log("path,index:",path,index);
     currentIndex.value=index;
     router.push(path);
 }
+onMounted(()=>{
+    ToRoute("/admin/flightManage",1);
+})
+onBeforeMount(()=>{
+    //组件挂载前，监听折叠侧边栏事件
+    emitter.on('collapseAside',(isCollapse)=>{
+        console.log("触发了collapseAside事件，传来的值为",isCollapse);
+        asideWidth.value=isCollapse?0:150;
+        console.log("asideWidth.value",asideWidth.value);
+    })
+})
 </script>
 
 <template>
     <div class="container">
-        <div class="aside">
+        <div class="aside" :style="{width:`${asideWidth}px`}">
             <div :class="{'aside-item':true,'aside-item-active':currentIndex===1}" @click="ToRoute('/admin/flightManage',1)">
                 <div class="aside-item-icon">
                     <svg t="1711280019456" class="icon" viewBox="0 0 2039 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7871" width="32" height="32"><path d="M1810.842002 53.260059c-58.284593 0-114.559372 13.063788-167.819431 37.18155-131.642787 64.314033-265.295388 125.613346-399.952895 182.893033-4.019627 2.009814-8.039254 2.009814-12.058881 2.009813-250.221786-29.142296-499.438665-59.2895-749.660452-86.421982-38.186457-6.029441-69.338567 19.093229-90.441609 35.171737-24.117763 17.083415-48.235525 35.171737-72.353287 52.255152-37.181551 25.122669-15.073602 56.274779 7.034347 65.31894L723.532875 528.580962c27.132483 15.073602 10.049068 36.176644 0 43.210991l-211.030422 92.451423c-6.029441 2.009814-13.063788 2.009814-19.093228 0-86.421982-38.186457-200.981354-65.31894-272.329735-89.436703-41.201178-14.068695-84.412169-2.009814-109.534838 24.117763-13.063788 13.063788-21.103042 30.147203-23.112856 48.235525-2.009814 18.088322 5.024534 36.176644 19.093229 48.235525l239.167811 207.010795c27.132483 23.112856 63.309127 21.103042 90.44161 20.098135 60.294406-2.009814 96.47105-4.019627 154.755642-9.044161 33.161923-3.01472 66.323847-12.058881 96.47105-26.127576 442.158979-216.054956 1268.192345-643.140334 1268.192346-643.140334 63.309127-37.181551 75.368008-86.421982 46.225711-117.574092-31.15211-37.181551-101.495584-73.358194-191.937193-73.358194z" p-id="7872" fill="#767c88"></path></svg>
@@ -48,8 +58,8 @@ function ToRoute(path,index)
 }
 .aside
 {
-    width: 150px;
-    background-color: rgba(255,255,255);
+    width: 130px;
+    background-color: #ffffff;
     padding-top: 10px;
     color: rgb(38,40,36);
     font-family: "Inter", sans-serif;
@@ -60,13 +70,11 @@ function ToRoute(path,index)
     position: sticky;
     top: 0;
     bottom: 0;
+    z-index: 1;
+    transition: width 0.3s ease;
+    //border-right: 2px solid red;
 }
-.aside:after
-{
-    content: "";
-    border-bottom: 2px solid #eef1f6;
-    width: 400px;
-}
+/*TODO: 完成aside与main之间的分割线*/
 .aside-item
 {
     display: flex;
@@ -104,5 +112,9 @@ function ToRoute(path,index)
 }
 .main
 {
+    flex: 1;
+    background-color: #ffffff;
+    padding: 30px 0 30px 30px;
+    z-index: 10;
 }
 </style>
