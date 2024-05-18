b0db305523cdc0a9a4a77b7b0fdba3e3dc7f9caf
Tohka <1324419148@qq.com>
2024-05-18 17:04:52 +0800

    feat: 添加处理错误的回调函数的处理逻辑

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,8 +15,13 @@ server.interceptors.request.use((config)=>{
 //响应拦截器
 server.interceptors.response.use((response)=>{
     return response;
-},(error)=>{
-    return Promise.reject(error);
+},(err)=>{
+    console.log("这里是axios的响应拦截器的处理错误的回调函数");
+    console.log("查看错误是：",err);
+    return Promise.reject({
+        error:err.response.data.error,
+        status:err.response.status
+    });
 })
 
 export  default server;
