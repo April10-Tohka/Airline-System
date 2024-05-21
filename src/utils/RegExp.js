61d3cbf4f4021151d73df0ed814376a99421c6c2
Tohka <1324419148@qq.com>
2024-05-21 17:05:29 +0800

    feat(utils): 添加匹配验证码的正则表达式

--- src/utils/RegExp.js
+++ src/utils/RegExp.js
@@ -10,3 +10,14 @@ export function phoneReg(phone)
     const phoneReg = /^1[3-9]\d{9}$/;
     return phoneReg.test(phone);
 }
+
+/**
+ * 匹配验证码是否为6位数字的正则表达式
+ * @param captcha 验证码
+ * @returns {boolean} 验证码是否正确
+ */
+export function captchaReg(captcha)
+{
+    const regex = /^\d{6}$/;
+    return regex.test(captcha);
+}
