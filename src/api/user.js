5de108751f085ab32205e2a119bbc691a89f3497
Tohka <1324419148@qq.com>
2024-05-25 17:51:35 +0800

    feat(api): 创建注册时与验证码相关的api

--- src/api/user.js
+++ src/api/user.js
@@ -14,3 +14,34 @@ export function accountLoginApi(data)
         data
     })
 }
+
+
+/**
+ * 获取注册验证码
+ * @param phone 手机号
+ * @returns {*}
+ */
+export function getRegisterCaptchaApi(phone)
+{
+    return request({
+        url:"/auth/register/send-captcha",
+        method:"post",
+        data:{
+            phone
+        }
+    })
+}
+
+/**
+ * 注册时校验手机号验证码
+ * @param data {{phone,captcha}}
+ * @returns {*}
+ */
+export function verifyCaptchaApi(data)
+{
+    return request({
+        url:"/auth/register/verify-captcha",
+        method:"post",
+        data
+    })
+}
