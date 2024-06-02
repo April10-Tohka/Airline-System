e721198b893bacf206b7194a36599bce0f0db39f
Tohka <1324419148@qq.com>
2024-06-02 17:10:03 +0800

    feat: 添加--FooterLayer组件

---
+++ src/layout/components/FooterLayer.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="footer">
+    <div>
+
+    </div>
+</div>
+</template>
+
+<style scoped>
+.footer
+{
+    margin-top: 40px;
+    background-color: #f8fafd;
+}
+</style>
