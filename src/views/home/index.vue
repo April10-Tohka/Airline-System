f2b76e04ffdb24661748099ec7822de56dc24942
Tohka <1324419148@qq.com>
2024-06-03 16:48:06 +0800

    delete: 删除目前2024-6-3觉得不需要的，后续再重构

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,63 +2,7 @@
 import NavigationBar from "@/components/NavigationBar.vue";
 import Carousel from "./components/Carousel.vue";
 import SearchForm from "@/views/home/components/SearchForm.vue";
-import {ref,onBeforeMount} from "vue";
-import {useRouter} from "vue-router";
-import {getColumnData,getHotFlight} from "@/api/flight.js"
-const flights=ref([]);//热门航线
-const router=useRouter();
-function ToTicket(departCity,arriveCity,flightDate)
-{
-    router.push({path:"/ticket",query:{departCity,arriveCity,flightDate}});
-}
-/**
-//获取数据并本地存储
-function getDataToLocalStorage()
-{
-    if(localStorage.getItem('dictionary'))
-    {
-        console.log("有数据");
-    }
-    else
-    {
-        console.log("无数据");
-        Promise.all([getColumnData('departCity'),getColumnData('airlineCompanyName'),getColumnData('aircraftType')])
-            .then(response=>{
-                console.log("response:",response);
-                const dictionary={
-                    cityList:response[0]?.data.data,
-                    airlineCompanyNameList:response[1]?.data.data,
-                    aircraftTypeList:response[2]?.data.data
-                }
-                localStorage.setItem('dictionary',JSON.stringify(dictionary));
-            })
-    }
 
-}
-onBeforeMount(()=>{
-    //首页挂载前获取一些数据,(城市、航空公司、飞机机型、热门航线）
-    getDataToLocalStorage();
-    getHotFlight().then(res=>{
-        let {data}=res.data;
-        for(let i=0;i<4;i++)
-        {
-            data[i].img=`https://mp-b31258f4-397c-40ca-87bd-b790755e323b.cdn.bspapp.com/Airline-System/img/img/recommend/hot-${i+1}.jpg`
-        }
-        console.log("获取到的热门航线",data);
-        flights.value=data;
-    })
-})
-/
-
-/**
- * 交换出发地和目的地
- */
-function swapFromTo()
-{
-    console.log("点击了交换目的地");
-    let [switchIcon]=document.getElementsByClassName("switch-icon");
-    switchIcon.classList.toggle("switch-icon-rotate");
-}
 </script>
 
 <template>
@@ -66,26 +10,6 @@ function swapFromTo()
     <NavigationBar></NavigationBar>
     <SearchForm></SearchForm>
     <Carousel></Carousel>
-
-
-
-<!--
-
-    <div class="hottitle">———— 热门航线 ————</div>
-    <button @click="router.push('/admin')">点击前往后台管理</button>
-    <div class="hot">
-        <el-row :gutter="30">
-            <el-col :span="6" v-for="(flight,index) in flights" :key="index">
-                <div class="el-card" @click="ToTicket(flight.departCity,flight.arriveCity,flight.flightDate)">
-                    <img :src="flight.img" alt />
-                    <span class="adr">{{flight.departCity}} - {{flight.arriveCity}}</span>
-                    <span>经济舱单程含税</span>
-                    <span>￥{{flight.price}}起</span>
-                </div>
-            </el-col>
-        </el-row>
-    </div>
-   -->
 </div>
 </template>
 
@@ -97,51 +21,6 @@ function swapFromTo()
     width: 1180px;
     margin: 0 auto;
     box-sizing: border-box;
-}
-.searchBox{
-  position: absolute;
-  margin-top: -400px;
-  margin-left: 150px;
-  z-index: 2;
-  opacity: 0.95;
-}
-.hottitle {
-  margin: 3rem auto;
-  text-align: center;
-  font-size: 2rem;
-  font-weight: 300;
-}
-.hot img {
-  width: 100%;
-}
-.hot .el-col{
-  text-align:left;
-}
-.hot .el-col>div>span{
-  display:block;
-  padding-left:10px;
-}
-.hot .el-col>div>span.adr{
-  font-size:1rem;
-  font-weight:700;
-  margin:10px 0;
-}
-.hot .el-col>div>span:last-child{
-  font-size:1.3rem;
-  font-weight:700;
-  color:#f00;
-  padding-left:6px;
-  margin-bottom:40px;
-}
-.hot .el-col .el-card {
-  border:2px solid #eeeeee;
-}
-.hot .el-col .el-card:hover{
-  border-color:#051039;
-  cursor: pointer;
-}
-.hot {
-  margin: 50px 15px;
 }
 
 </style>
