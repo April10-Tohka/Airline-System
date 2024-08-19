92545ea7c6a1e8fa28f91c1c3ac0dcf25c6f549c
Tohka <1324419148@qq.com>
2024-08-19 23:37:51 +0800

    style:
    删除layout布局下的footer组件

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,38 +1,29 @@
 <script setup>
 import TopLayer from "@/layout/components/TopLayer.vue";
-import FooterLayer from "@/layout/components/FooterLayer.vue";
 </script>
 
 <template>
-    <el-container>
-        <el-header class="el-header">
-            <TopLayer></TopLayer>
-        </el-header>
-        <el-main class="el-main">
-            <router-view></router-view>
-        </el-main>
-        <el-footer class="el-footer">
-            <FooterLayer></FooterLayer>
-        </el-footer>
-    </el-container>
+  <el-container>
+    <el-header class="el-header">
+      <TopLayer></TopLayer>
+    </el-header>
+    <el-main class="el-main">
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
-.el-header,.el-main,.el-footer
-{
-    padding: 0;
-    margin: 0;
+.el-header,
+.el-main {
+  padding: 0;
+  margin: 0;
 }
 .el-header {
-    height: fit-content;
+  height: fit-content;
 }
 .el-main {
-    min-height: calc(100vh - 210px);
-    background-color: #eef1f6;
+  min-height: calc(100vh - 210px);
+  background-color: #eef1f6;
 }
-.el-footer {
-    height: fit-content;
-    background-color: #f8fafd;
-}
-
 </style>
