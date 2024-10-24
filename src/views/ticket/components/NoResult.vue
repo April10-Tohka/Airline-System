0d0f87aa2933c029d4c506210bfbb71c786419f0
Tohka <1324419148@qq.com>
2024-10-24 16:57:40 +0800

    feat: 添加NoResult组件，用于查询不到时，显示该组件来提醒用户

---
+++ src/views/ticket/components/NoResult.vue
@@ -0,0 +1,49 @@
+<script setup>
+const props = defineProps({
+  depart: {
+    type: String,
+    required: true,
+  },
+  arrive: {
+    type: String,
+    required: true,
+  },
+  departDate: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="no-result">
+    <i class="no-result-icon"></i>
+    <div class="title">
+      您查询的 {{ props.depart }} 至 {{ props.arrive }}（出发日期：{{
+        props.departDate
+      }}） 的机票可能因无航班或航班座位已售完导致暂时无法查询到对应价格。
+      建议您更换旅行日期或旅行城市重新查询
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.no-result {
+  position: relative;
+  margin-bottom: 4px;
+  background-color: #fff9ea;
+  min-height: 50px;
+  padding: 15px 15px 15px 100px;
+}
+.no-result-icon {
+  width: 58px;
+  height: 58px;
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  margin-top: -29px;
+  background-image: url("https://pic.c-ctrip.com/flight_intl/list/ico_sprite@2x.png?v6");
+  background-position: -580px -370px;
+  background-repeat: no-repeat;
+}
+</style>
--- src/views/ticket/index.vue
+++ src/views/ticket/index.vue
@@ -3,184 +3,241 @@
 import SearchForm from "@/views/home/components/SearchForm.vue";
 import FlightItem from "@/views/ticket/components/FlightItem.vue";
 import TopBar from "@/views/ticket/components/TopBar.vue";
-import BookConfirmation from "./components/BookConfirmation.vue"
+import BookConfirmation from "./components/BookConfirmation.vue";
+import NoResult from "@/views/ticket/components/NoResult.vue";
 // 引入方法
-import {getFormattedDate} from "@/utils/dateFormat.js"
-import {onMounted, ref} from "vue";
-import {useRoute,useRouter} from "vue-router";
-import {getFlightTickets} from "@/api/flight.js";
+import { getFormattedDate } from "@/utils/dateFormat.js";
+import { onMounted, ref } from "vue";
+import { getFlightSchedule } from "@/api/flight.js";
+import { useFlightStore } from "@/stores/modules/flight.js";
+const flightStore = useFlightStore();
 
-onMounted(()=>{
-    loading.value=true;
-    getFlightTickets(route.query.depart,route.query.arrival)
-        .then(res=>{
-            flightTickets.value=res.data.data;
-            setTimeout(()=>{
-                loading.value=false;
-            },800)
-        })
-})
-const route=useRoute();
-const router=useRouter();
-const flightTickets=ref([]);//飞机票
+onMounted(() => {
+  //显示骨架屏
+  loading.value = true;
+  getFlightSchedule(flightStore.flight).then((data) => {
+    const { flights, total } = data;
+    if (total === 0) {
+      setTimeout(() => {
+        loading.value = false;
+        isRenderNoResult.value = true;
+      }, 400);
+      return;
+    }
+    flightTickets.value = flights;
+    isRenderNoResult.value = false;
+    setTimeout(() => {
+      loading.value = false;
+    }, 400);
+  });
+});
 
-//ticket页面中，点击搜索按钮，获取对应航班
-function queryFlights(value)
-{
-    loading.value=true;
-    console.log("ticket组件传递的,收到的参数value:",value);
-    router.push({path:"/ticket",query:value});
-    getFlightTickets(value.depart,value.arrival)
-        .then(res=>{
-            flightTickets.value=res.data.data;
-            setTimeout(()=>{
-                loading.value=false;
-            },800)
-        })
-}
-//是否显示订单弹窗
-let showOrder=ref(false);
-//选择的飞机票
-let selectTicket=ref(null);
-
-//点击订票的处理逻辑
-function handleShowOrder(args)
-{
-    console.log("处理ShowOrder事件的回调函数",args);
-    showOrder.value=args.showOrder;
-    selectTicket.value=args.selectTicket;
-}
+const props = defineProps([
+  "arriveCity",
+  "departCity",
+  "departDate",
+  "departCityCode",
+  "arriveCityCode",
+]);
 
 //是否显示骨架屏
-const loading=ref(false);
+const loading = ref(false);
+const flightTickets = ref([]); //飞机票
+
+//是否显示订单弹窗
+const showOrder = ref(false);
+//选择的飞机票
+const selectTicket = ref(null);
+const isRenderNoResult = ref(false); //是否渲染无航班组件
+//点击订票的处理逻辑
+function handleShowOrder(args) {
+  console.log("处理ShowOrder事件的回调函数", args);
+  showOrder.value = args.showOrder;
+  selectTicket.value = args.selectTicket;
+}
+const queryFlightSchedule = () => {
+  //显示骨架屏
+  loading.value = true;
+  getFlightSchedule(flightStore.flight).then((data) => {
+    const { flights, total } = data;
+    if (total === 0) {
+      setTimeout(() => {
+        loading.value = false;
+        isRenderNoResult.value = true;
+      }, 400);
+      return;
+    }
+    flightTickets.value = flights;
+    isRenderNoResult.value = false;
+    setTimeout(() => {
+      loading.value = false;
+    }, 400);
+  });
+};
 </script>
 
 <template>
-    <SearchForm :on-click="queryFlights"></SearchForm>
-    <div class="body-wrapper">
-        <div class="result-header">
-            <div class="address">
-                <span class="depart">{{route.query.depart}}</span>
-                <svg t="1710934823512" class="arrow" viewBox="0 0 6016 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5180" width="32" height="32"><path d="M5043.2 768V179.2c0-76.8 51.2-128 128-128 38.4 0 64 12.8 89.6 38.4l716.8 716.8c51.2 51.2 51.2 128 0 179.2-25.6 25.6-51.2 38.4-89.6 38.4H179.2c-76.8 0-128-51.2-128-128s51.2-128 128-128h4864z" fill="#333333" p-id="5181"></path></svg>
-                <span class="arrival">{{route.query.arrival}}</span>
-            </div>
-            <div class="depart-date">{{getFormattedDate(route.query.departDate)}}</div>
-        </div>
-<!--        顶部工具栏-->
-        <TopBar></TopBar>
-<!--        飞机票-->
-        <el-skeleton animated :loading="loading" :count="6">
-            <template #template>
-                <div class="fake-flight-summary">
-                    <div class="fake-flight-airline">
-                        <el-skeleton-item variant="text" style="width: 40px;height: 40px;"></el-skeleton-item>
-                        <div style="flex-direction: column;display: flex;position: relative;left: 10px;">
-                            <el-skeleton-item variant="h3" style="width: 80px;height: 20px;"></el-skeleton-item>
-                            <el-skeleton-item variant="text" style="width: 80px;height: 10px;margin-top: 10px"></el-skeleton-item>
-                        </div>
-                    </div>
-                    <div class="fake-flight-detail">
-                        <el-skeleton-item variant="h1" style="width: 295px;height: 20px;"></el-skeleton-item>
-                    </div>
-                    <div class="fake-flight-operate">
-                        <el-skeleton-item variant="button" style="width: 80px;height: 20px"></el-skeleton-item>
-                    </div>
-                </div>
-            </template>
-            <template #default>
-                <FlightItem :flight-tickets="flightTickets" @showOrder="handleShowOrder"></FlightItem>
-            </template>
-        </el-skeleton>
-
+  <SearchForm @query-flight-schedule="queryFlightSchedule"></SearchForm>
+  <div class="body-wrapper">
+    <div class="result-header">
+      <div class="address">
+        <span class="depart">{{ departCity }}</span>
+        <svg
+          t="1710934823512"
+          class="arrow"
+          viewBox="0 0 6016 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="5180"
+          width="32"
+          height="32"
+        >
+          <path
+            d="M5043.2 768V179.2c0-76.8 51.2-128 128-128 38.4 0 64 12.8 89.6 38.4l716.8 716.8c51.2 51.2 51.2 128 0 179.2-25.6 25.6-51.2 38.4-89.6 38.4H179.2c-76.8 0-128-51.2-128-128s51.2-128 128-128h4864z"
+            fill="#333333"
+            p-id="5181"
+          ></path>
+        </svg>
+        <span class="arrival">{{ arriveCity }}</span>
+      </div>
+      <div class="depart-date">
+        {{ getFormattedDate(departDate) }}
+      </div>
     </div>
-<!--    确认订单窗口-->
-    <BookConfirmation v-model="showOrder" :ticket="selectTicket"></BookConfirmation>
+
+    <!--        飞机票-->
+    <el-skeleton animated :loading="loading" :count="6">
+      <template #template>
+        <div class="fake-flight-summary">
+          <div class="fake-flight-airline">
+            <el-skeleton-item
+              variant="text"
+              style="width: 40px; height: 40px"
+            ></el-skeleton-item>
+            <div
+              style="
+                flex-direction: column;
+                display: flex;
+                position: relative;
+                left: 10px;
+              "
+            >
+              <el-skeleton-item
+                variant="h3"
+                style="width: 80px; height: 20px"
+              ></el-skeleton-item>
+              <el-skeleton-item
+                variant="text"
+                style="width: 80px; height: 10px; margin-top: 10px"
+              ></el-skeleton-item>
+            </div>
+          </div>
+          <div class="fake-flight-detail">
+            <el-skeleton-item
+              variant="h1"
+              style="width: 295px; height: 20px"
+            ></el-skeleton-item>
+          </div>
+          <div class="fake-flight-operate">
+            <el-skeleton-item
+              variant="button"
+              style="width: 80px; height: 20px"
+            ></el-skeleton-item>
+          </div>
+        </div>
+      </template>
+      <template #default>
+        <div v-if="isRenderNoResult">
+          <NoResult
+            :depart-date="flightStore.departDate"
+            :arrive="flightStore.arrive.name"
+            :depart="flightStore.depart.name"
+          ></NoResult>
+        </div>
+        <div v-else>
+          <!--        顶部工具栏-->
+          <TopBar></TopBar>
+          <FlightItem
+            :flight-tickets="flightTickets"
+            @showOrder="handleShowOrder"
+          ></FlightItem>
+        </div>
+      </template>
+    </el-skeleton>
+  </div>
+  <!--    确认订单窗口-->
+  <BookConfirmation
+    v-model="showOrder"
+    :ticket="selectTicket"
+  ></BookConfirmation>
 </template>
-
-
-
-
 
 <style scoped lang="scss">
 $padding-left: 33px;
-.body-wrapper
-{
-    max-width: 1180px;
-    margin: 0 auto;
+.body-wrapper {
+  max-width: 1180px;
+  margin: 0 auto;
 }
-.result-header
-{
-    margin-bottom: 4px;
+.result-header {
+  margin-bottom: 4px;
+  display: flex;
+  flex-direction: row;
+  padding-left: $padding-left;
+  align-items: center;
+  line-height: 1;
+  height: 50px;
+  .address {
     display: flex;
     flex-direction: row;
-    padding-left: $padding-left;
     align-items: center;
-    line-height: 1;
-    height: 50px;
-    .address
-    {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        .depart,.arrival
-        {
-            font-weight: 700;
-            font-size: 24px;
-        }
-        .arrow
-        {
-            margin: 0 4px;
-        }
+    .depart,
+    .arrival {
+      font-weight: 700;
+      font-size: 24px;
     }
-    .depart-date
-    {
-        color: #333333;
-        font-size: 18px;
-        margin-left: 10px;
+    .arrow {
+      margin: 0 4px;
     }
+  }
+  .depart-date {
+    color: #333333;
+    font-size: 18px;
+    margin-left: 10px;
+  }
 }
 /*骨架屏样式*/
-.body-wrapper:deep(.el-skeleton)
-{
-    box-sizing: border-box;
-    margin-top: 4px;
-    padding-left: 33px;
-    height: 80px;
+.body-wrapper:deep(.el-skeleton) {
+  box-sizing: border-box;
+  margin-top: 4px;
+  padding-left: 33px;
+  height: 80px;
 }
-.fake-flight-summary
-{
+.fake-flight-summary {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #f8fafd;
+  height: 100%;
+  position: relative;
+  .fake-flight-airline {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    background-color: #f8fafd;
-    height: 100%;
-    position: relative;
-    .fake-flight-airline
-    {
-        display: flex;
-        flex-direction: row;
-        position: absolute;
-        top: 20px;
-        left: 24px;
-    }
-    .fake-flight-detail
-    {
-        position: absolute;
-        left: 395px;
-        top: 30px
-    }
-    .fake-flight-operate
-    {
-        position: absolute;
-        top: 30px;
-        right: 20px;
-    }
+    position: absolute;
+    top: 20px;
+    left: 24px;
+  }
+  .fake-flight-detail {
+    position: absolute;
+    left: 395px;
+    top: 30px;
+  }
+  .fake-flight-operate {
+    position: absolute;
+    top: 30px;
+    right: 20px;
+  }
 }
-.fake-flight-summary:nth-child(3n+1)
-{
-    background-color: #ffffff;
+.fake-flight-summary:nth-child(3n + 1) {
+  background-color: #ffffff;
 }
-
-
 </style>
-
