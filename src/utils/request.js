31a837fad06399719d03eb56f2f5e2a36af077fd
Tohka <1324419148@qq.com>
2024-10-26 17:33:56 +0800

    feat: 添加处理401 和403 错误逻辑
    处理401会进行刷新token，处理403会跳转到login登录页

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/modules/auth.js";
 const baseURL = "http://localhost:3000";
 const server = axios.create({
   baseURL,
@@ -8,73 +10,51 @@ const server = axios.create({
 server.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
-    console.log("=>(request.js:14) 发送请求前");
+    console.log("=>(request.js:14) 发送请求前", config);
+    config.headers.setAuthorization(`Bearer ${useAuthStore().accessToken}`);
     return config;
   },
-  function (error) {
+  function (err) {
     // 对请求错误做些什么
     console.log("=>(request.js:20) 发送请求错误");
-    return Promise.reject(error);
+    return Promise.reject(err);
   },
 );
 
 // 添加响应拦截器
 server.interceptors.response.use(
-  function (response) {
+  function (res) {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
-    console.log("=>(request.js:30) 响应成功", response);
-    return response.data.data;
+    console.log("=>(request.js:30) 响应成功", res);
+    return res.data.data;
   },
-  function (error) {
+  function (err) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
-    console.log("=>(request.js:38) 响应失败", error);
-    return Promise.reject(error.response.data.message);
-
-    if (!error.response) {
-      console.error("Network Error:", error.message);
-      alert("网络错误，请稍后再试");
-      return;
-    }
-    const status = error.response.status; //获取http状态码
+    console.log("=>(request.js:38) 响应失败", err);
+    const status = err.response.status; //获取http状态码
+    const config = err.config; //获取请求配置config
     switch (status) {
-      case 400:
-        console.error(
-          "Bad Request:",
-          error.response.data.message || "请求错误",
-        );
-        alert("请求错误，请检查输入");
-        break;
       case 401:
-        console.error("Unauthorized:", error.response.data.message || "未授权");
-        alert("未授权，请登录");
-        // 重定向到登录页，或者触发退出登录逻辑
-        break;
+        console.error("Unauthorized 未授权", err.response.data.error.message);
+        //使用refreshToken 去刷新token
+        return useAuthStore()
+          .refreshAccessTokenAndRefreshToken()
+          .then(() => {
+            // 刷新token成功后继续原来的操作
+            console.log("通知了刷新双token成功，所以我去继续原来的操作");
+            return server(config);
+          });
       case 403:
-        console.error("Forbidden:", error.response.data.message || "禁止访问");
-        alert("禁止访问，您没有权限");
-        break;
-      case 404:
-        console.error(
-          "Not Found:",
-          error.response.data.message || "资源未找到",
-        );
-        alert("资源未找到，请检查 URL");
-        break;
-      case 500:
-        console.error(
-          "Server Error:",
-          error.response.data.message || "服务器错误",
-        );
-        alert("服务器错误，请稍后再试");
-        break;
+        console.error("403情况下，refreshToken 不可信，要重新登录");
+        // 跳转到登录页
+        window.location.href = "/login";
+        return Promise.reject("跳转到login重新登录");
+
       default:
-        console.error(
-          "Unexpected Error:",
-          error.response.data.message || "未知错误",
-        );
-        alert("未知错误，请稍后再试");
+        console.log("status状态码不再在以上情况");
+        return Promise.reject(err.response.data.message);
     }
   },
 );
